f07154af0ce379584221046bd0914b3c5ad8e43f
Endermandels <elijahdelavar@gmail.com>
2024-05-28 15:46:44 -0700

    Implement WASD movement

--- Player/player.gd
+++ Player/player.gd
@@ -2,14 +2,20 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-func _physics_process(delta):
-	var x_direction = Input.get_axis("ui_left", "ui_right")
+func _physics_process(_delta):
+	var x_direction = Input.get_axis('ui_left', 'ui_right')
 	var y_direction = Input.get_axis('ui_up', 'ui_down')
+	
+	# Move if input, otherwise slow down
 	if x_direction:
 		velocity.x = x_direction * SPEED
-		velocity.y = y_direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
 
+	# Move if input, otherwise slow down
+	if y_direction:
+		velocity.y = y_direction * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
 	move_and_slide()
